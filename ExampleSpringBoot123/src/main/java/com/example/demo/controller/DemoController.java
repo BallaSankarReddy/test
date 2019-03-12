ff999288daf5753b33818dd8edc4e9d70220e863
cruvz <cruvz@DESKTOP-JUECAKE>
2019-03-12 22:24:18 +0530

    Added controllere class

---
+++ ExampleSpringBoot123/src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	@RequestMapping("/sayHello")
+	public String sayHello() {
+		return "Calling from DemoController Class..";
+	}
+
+	
+}
